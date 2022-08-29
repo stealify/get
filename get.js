33aaa177425b8f2698d8ea6f28978db4ff1fedac
Frank Lemanschik <frank@lemanschik.com>
2022-08-29 12:02:19 +0200

    Update get.js
    Add first iteration of long string support #1

--- get.js
+++ get.js
@@ -1,4 +1,5 @@
 // (createTemplate`${0} ${'foo'}!`)('Hello', {foo: 'World'}); // "Hello World!"
+const longStringSupportForDownloads = (s) => s.replaceAll("\t",'').replaceAll("\r",'').replaceAll("\n",'').replaceAll(" ",' ')
 const createTemplate = (strings, ...keys) => 
   (...values) => {
     const dict = (values[values.length - 1] || {});
@@ -7,7 +8,7 @@ const createTemplate = (strings, ...keys) =>
       result.push(Number.isInteger(key) 
         ? values[key] : dict[key], strings[i + 1])
     );
-    return result.join('');
+    return longStringSupportForDownloads(result.join(''));
   };
 
 export { createTemplate };
